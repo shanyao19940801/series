3c06cd50f1116cc8de5bc569668af337eaaed58a
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-12 21:02:16 +0800

    代理

---
+++ series/proxys/src/main/java/com/eyao/proxys/ProxysApplication.java
@@ -0,0 +1,12 @@
+package com.eyao.proxys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxysApplication.class, args);
+    }
+}
