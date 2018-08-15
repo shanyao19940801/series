bc2e6064427f4277a411cacafb7c40d7a6d65c3e
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-15 23:59:31 +0800

    消费服务

--- series/muse-consumer/src/main/java/com/yao/museconsumer/MuseConsumerApplication.java
+++ series/muse-consumer/src/main/java/com/yao/museconsumer/MuseConsumerApplication.java
@@ -3,6 +3,8 @@ package com.yao.museconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +13,10 @@ public class MuseConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MuseConsumerApplication.class, args);
     }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 }
