9176564d1241f3f93ca5aa6d673cd7b0739b0ad6
shanyao <sqshanyao@163.com>
2018-08-16 18:16:35 +0800

    爬取信息

--- series/muse-spider/src/main/java/com/yao/musespider/parser/NcarFinishedSerListParser.java
+++ series/muse-spider/src/main/java/com/yao/musespider/parser/NcarFinishedSerListParser.java
@@ -1,11 +1,17 @@
 package com.yao.musespider.parser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.util.List;
 
 public class NcarFinishedSerListParser implements IPageParser<NcarFinishedSerListParser> {
     @Override
     public List<NcarFinishedSerListParser> parser(String html) {
         System.out.println("test");
+        Document document = Jsoup.parse(html);
+        Elements elements = document.select("th.new");
         return null;
     }
 }
---
+++ series/muse-spider/src/test/java/com/yao/musespider/parser/NcarFinishedSerListParserTest.java
@@ -0,0 +1,14 @@
+package com.yao.musespider.parser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NcarFinishedSerListParserTest {
+
+    @Test
+    public void parser() {
+        NcarFinishedSerListParser parser = new NcarFinishedSerListParser();
+        parser.parser("");
+    }
+}
