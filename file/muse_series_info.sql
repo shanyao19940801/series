738c67a36f3ea95864eb8d2ac3b1781cace07439
shanyao <sqshanyao@163.com>
2018-08-20 15:39:14 +0800

    整合mybatis

---
+++ file/muse_series_info.sql
@@ -0,0 +1,10 @@
+create table muse_series_info(
+	id int primary key comment 'id',
+	name varchar(20) comment '名称',
+	page_url varchar(200) comment '每集地址',
+	type int default 3 comment '类型 3:电视剧，4:电影',
+	country varchar(20) comment '国家',
+	finish int default 1 comment '是否完结 1：完结，0：未完结',
+	inserttime timestamp  comment '插入时间',
+	updatetime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间'
+)
