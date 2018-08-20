472602a6e64c04d7efd665111c1d272e148e4d6c
shanyao <sqshanyao@163.com>
2018-08-20 18:13:08 +0800

    no message

--- series/muse-spider/src/main/java/com/yao/musespider/controller/TestController.java
+++ series/muse-spider/src/main/java/com/yao/musespider/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.yao.musespider.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/spider")
 public class TestController {
 
+    @GetMapping(value = "/test")
+    public String getTest(){
+        return "test shanyap";
+    }
 }
