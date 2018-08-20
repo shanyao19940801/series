944e3f1f95b6cd3e45d22da84845657ec81a57bf
shanyao <sqshanyao@163.com>
2018-08-20 18:09:14 +0800

    mybatis整合

---
+++ series/muse-spider/src/main/java/com/yao/musespider/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.yao.musespider.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/spider")
+public class TestController {
+
+}
