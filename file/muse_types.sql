cb06c9c29a2d8abd8f0321e6480fec50e9e7dbbe
shanyao <sqshanyao@163.com>
2018-08-22 18:07:16 +0800

    枚举

--- file/muse_types.sql
+++ file/muse_types.sql
@@ -1,5 +1,5 @@
 create table muse_data_dict(
-	id int primary key,
+	id int primary key auto_increment comment 'id',
 	name varchar(20) comment '美剧名称',
 	value int comment '枚举值',
 	type varchar(20) comment '枚举类型',
@@ -7,4 +7,15 @@ create table muse_data_dict(
 	updatetime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间'#如果不插入则自动插入系统时间	
 );
 
-alter table muse_data_dict auto_increment = 1000;
+alter table muse_data_dict auto_increment = 1000
+
+
+insert into muse_data_dict(name,value,type,inserttime) value('电视剧',1,'影视类型',now()),('电影',2,'影视类型',now())
+		,('日韩',1,'地区',now())
+,('欧美',2,'地区',now())
+,('完结',1,'影视状态',now())
+,('连载',2,'影视状态',now())
+,('种子',1,'资源类型',now())
+,('百度云',2,'资源类型',now())
+,('标清',1,'清晰度',now())
+,('高清',2,'清晰度',now())
