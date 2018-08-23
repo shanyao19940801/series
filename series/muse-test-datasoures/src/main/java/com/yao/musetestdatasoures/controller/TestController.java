84c1acdc76c27bfc81f4761fae31d7c9b71623a7
shanyao <sqshanyao@163.com>
2018-08-23 18:05:17 +0800

    多数据源

--- series/muse-test-datasoures/src/main/java/com/yao/musetestdatasoures/controller/TestController.java
+++ series/muse-test-datasoures/src/main/java/com/yao/musetestdatasoures/controller/TestController.java
@@ -1,4 +1,24 @@
 package com.yao.musetestdatasoures.controller;
 
+import com.yao.musetestdatasoures.dao.zentao.ProxyMapper;
+import com.yao.musetestdatasoures.entity.zentao.Proxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class TestController {
+    @Autowired
+    ProxyMapper proxyMapper;
+    @RequestMapping(value = "/test")
+    public String test() {
+        return "test";
+    }
+
+    @RequestMapping(value = "/proxy")
+    public String proxy() {
+        Proxy proxy = proxyMapper.selectById(1000);
+        return "";
+    }
 }
--- series/muse-test-datasoures/src/main/java/com/yao/musetestdatasoures/dao/zentao/ProxyMapper.java
+++ series/muse-test-datasoures/src/main/java/com/yao/musetestdatasoures/dao/zentao/ProxyMapper.java
@@ -10,13 +10,21 @@ public interface ProxyMapper {
 
     int deleteByExample(ProxyExample example);
 
+    int deleteByPrimaryKey(Integer id);
+
     int insert(Proxy record);
 
     int insertSelective(Proxy record);
 
     List<Proxy> selectByExample(ProxyExample example);
 
+    Proxy selectByPrimaryKey(Integer id);
+
     int updateByExampleSelective(@Param("record") Proxy record, @Param("example") ProxyExample example);
 
     int updateByExample(@Param("record") Proxy record, @Param("example") ProxyExample example);
+
+    int updateByPrimaryKeySelective(Proxy record);
+
+    int updateByPrimaryKey(Proxy record);
 }
