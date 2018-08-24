ad78db8cc6d908e0e6617c453e697779f84dcbb7
shanyao <sqshanyao@163.com>
2018-08-24 16:24:42 +0800

    多数据源

---
+++ series/muse-test-datasource1/src/main/java/com/yao/musetestdatasource1/MuseTestDatasource1Application.java
@@ -0,0 +1,12 @@
+package com.yao.musetestdatasource1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseTestDatasource1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseTestDatasource1Application.class, args);
+    }
+}
