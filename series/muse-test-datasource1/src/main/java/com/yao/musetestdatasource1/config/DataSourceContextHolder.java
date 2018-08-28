931768a909253ace4787b3e4af14767bc2eeb1bd
shanyao <sqshanyao@163.com>
2018-08-28 17:45:25 +0800

    动态数据源

--- series/muse-test-datasource1/src/main/java/com/yao/musetestdatasource1/config/DataSourceContextHolder.java
+++ series/muse-test-datasource1/src/main/java/com/yao/musetestdatasource1/config/DataSourceContextHolder.java
@@ -1,6 +1,6 @@
 package com.yao.musetestdatasource1.config;
 
-import com.netflix.servo.annotations.DataSourceType;
+import com.yao.musetestdatasource1.utils.DataSourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +16,21 @@ public class DataSourceContextHolder {
      * 读可能是多个库
      */
     public static void read() {
-//        local.set(DataSourceType.);
+        local.set(DataSourceType.read.getType());
+        System.out.println("==:" + DataSourceType.read.getType());
         logger.info("数据库切换到读库...");
     }
+
+    /**
+     * 写只有一个库
+     */
+    public static void write() {
+        local.set(DataSourceType.write.getType());
+        logger.info("数据库切换到写库...");
+    }
+
+    public static String getJdbcType(){
+        return local.get();
+    }
+
 }
